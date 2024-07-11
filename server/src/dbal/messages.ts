00bc02e51f33edc8ccad608751a15399395a893e
amandagalemmo <52899802+amandagalemmo@users.noreply.github.com>
2024-07-10 23:11:20 -0400

    remove stray logs and testing func

--- server/src/dbal/messages.ts
+++ server/src/dbal/messages.ts
@@ -28,12 +28,3 @@ export async function insertMessage(
 		msg
 	);
 }
-
-export function getHardcodedMessages(): string[] {
-	const messages = [
-		"Hello world!",
-		"What's updog?",
-		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula purus sit amet nisi lacinia, nec pretium lectus facilisis. Suspendisse eu ultricies ipsum. Nam ac mi et eros vestibulum ultricies. Praesent suscipit ullamcorper eros eget dignissim. Sed in erat ultricies, pretium orci a, maximus magna. Mauris mollis bibendum purus, ac lacinia dolor dapibus vel. Morbi diam velit, tristique sed eleifend ac, vestibulum vel quam. Proin eleifend blandit ligula, eget tempor ligula sagittis ut."
-	];
-	return messages;
-}
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 router.get('/', async function(req, res, next) {
   const db = req.app.get("db");
   const messageRows = await fetchAllMessages(db);
-  console.log("messageRows", messageRows);
 
   res.render(
     'index',
